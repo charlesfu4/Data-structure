c1d7a77a8555f481271c1bd26aafe3c18f61a578
Chu-Cheng Fu <ccfu@kth.se>
2019-07-22 11:59:20 +0200

    phonebook

--- Ass3/1_phone_book/phone_book.py
+++ Ass3/1_phone_book/phone_book.py
@@ -1,47 +1,44 @@
-# python3
+#python3
 
-class Query:
-    def __init__(self, query):
-        self.type = query[0]
-        self.number = int(query[1])
-        if self.type == 'add':
-            self.name = query[2]
-
-def read_queries():
-    n = int(input())
-    return [Query(input().split()) for i in range(n)]
-
-def write_responses(result):
-    print('\n'.join(result))
-
-def process_queries(queries):
-    result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
-    for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+class phonebook:
+    def __init__(self):
+        self.input = 0
+        self.queries = []
+        self.pbook = [None]*(10**7)#phone num with 7 digits
+    
+    def add(self, number, name):
+        self.pbook[number] = name
+    
+    def delete(self, number):
+        self.pbook[number] = None
+    
+    def find(self, number):
+        name = self.pbook[number]
+        if name == None:
+            return 'not found'
         else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
-    return result
+            return name
+    
+    #input queries processing
+    def inputprocess(self):
+        self.input = int(input())
+        self.queries = [input() for element in range(self.input)]
+        self.processq()
+    
+    #query individully processing
+    def processq(self):
+        for query in self.queries:
+            ele = query.split()
+            instruct, num = ele[0], int(ele[1])
+            if instruct == 'add':
+                self.add(num, ele[2])
+            elif instruct == 'del':
+                self.delete(num)
+            elif instruct == 'find':
+                print(self.find(num))
+
+
 
 if __name__ == '__main__':
-    write_responses(process_queries(read_queries()))
-
+    a = phonebook()
+    a.inputprocess()
